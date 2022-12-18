010fedfaa75320ecc047ab008975a14aa2436b20
Ao Gao <ao.gao@tum.de>
2022-12-18 20:33:15 +0100

    FEAT: Update debug feature

---
+++ Assets/Scripts/DebugController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugController : MonoBehaviour
+{
+    private bool active = false;
+    [SerializeField] List<GameObject> _debugObjects;
+
+    public void ToogleDebug()
+    {
+        foreach(GameObject g in _debugObjects)
+        {
+            g.SetActive(!active);
+        }
+            active = !active;
+    }
+}
